567e843b8fc093cece7e3c931aec2f23344fc2fa
YounesBenaggoune <younesbenaggoune50@gmail.com>
2023-01-01 14:29:03 -0800

    custom text-field component

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -1,36 +1,48 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React, { FC, ReactNode } from 'react';
+import React, { ReactNode, InputHTMLAttributes, forwardRef } from 'react';
+import cn from 'classnames';
 import './text-field.scss';
 
-type TextFieldProps = {
-  icon: ReactNode;
+interface TextFieldOptionalProps {
+  icon?: ReactNode;
+}
+interface TextFieldProps
+  extends TextFieldOptionalProps,
+    InputHTMLAttributes<HTMLInputElement> {
+  icon?: ReactNode;
   placeHolder: string;
   label: string;
   type: string;
+}
+
+const defaultProps: TextFieldOptionalProps = {
+  icon: '',
 };
 
-const TextField: FC<TextFieldProps> = ({
-  icon,
-  placeHolder,
-  label,
-  type,
-  ...props
-}: TextFieldProps) => {
-  return (
-    <div className="text-field">
-      <label htmlFor="password"> {label} </label>
-      <div className="text-field__wrp">
-        {icon}
-        <input
-          type={type}
-          name={label}
-          id={label}
-          placeholder={placeHolder}
-          {...props}
-        />
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  function TextField({ label, icon, type, placeHolder }, ref) {
+    return (
+      <div className="text-field">
+        <label htmlFor={label}> {label} </label>
+        <div
+          className={cn(
+            'text-field__wrapper',
+            icon && 'text-field__wrapper--icon'
+          )}
+        >
+          {icon}
+          <input
+            type={type}
+            name={label}
+            id={label}
+            placeholder={placeHolder}
+            ref={ref}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
+
+TextField.defaultProps = defaultProps;
 
 export default TextField;
